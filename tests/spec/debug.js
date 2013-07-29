33f3ae3763fd1d2ae48658f03e384dd4c03a2348
Benjamin Goering <bengoering@gmail.com>
2013-07-29 13:14:37 -0700

    Add tests for debug

---
+++ tests/spec/debug.js
@@ -0,0 +1,20 @@
+define(['jasmine', 'streamhub-sdk/debug'], function (jasmine, debug) {
+	describe('streamhub-sdk/debug', function () {
+		it('is defined as a function', function () {
+			expect(debug).toBeDefined();
+			expect(debug).toEqual(jasmine.any(Function));
+		});
+		describe('when executed', function () {
+			var log;
+			beforeEach(function () {
+				log = debug('logger name');
+			});
+			it('returns a function that doesnt throw', function () {
+				expect(log).toEqual(jasmine.any(Function));
+				expect(function () {
+					log('my message');
+				}).not.toThrow();
+			});
+		})
+	});
+});
