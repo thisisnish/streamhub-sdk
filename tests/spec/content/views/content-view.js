d80131f296677f479329b7bbc54c69bc44cf6333
Benjamin Goering <bengoering@gmail.com>
2013-08-02 15:47:08 -0700

    Update util.formatDate to calculate relative timestamp strings. Test

--- tests/spec/content/views/content-view.js
+++ tests/spec/content/views/content-view.js
@@ -13,6 +13,7 @@ function ($, jasmine, jasmineJquery, Content, LivefyreContent, ContentView) {
                 expect(contentView.createdAt instanceof Date).toBe(true);
             });
         });
+
         describe('when viewing LivefyreContent', function () {
             var livefyreContent = new LivefyreContent({"vis": 1, "content": {"replaces": "", "feedEntry": {"transformer": "lfcore.v2.procurement.feed.transformer.instagram", "feedType": 2, "description": "#gayrights #lgbt #equality #marriageequality <img src=\"http://distilleryimage2.instagram.com/18ea2500970c11e294f522000a9f30b8_7.jpg\" />", "pubDate": 1364409052, "channelId": "http://instagram.com/tags/marriageequality/feed/recent.rss", "link": "http://distilleryimage2.instagram.com/18ea2500970c11e294f522000a9f30b8_7.jpg", "id": "bffcb85a-2976-4396-bb60-3cf5b1e2c3a8", "createdAt": 1364409052}, "bodyHtml": "#gayrights #lgbt #equality #marriageequality ", "annotations": {}, "authorId": "7759cd005d95d8cc5bd93718b2ac0064@instagram.com", "parentId": "", "updatedAt": 1364409052, "id": "bffcb85a-2976-4396-bb60-3cf5b1e2c3a8", "createdAt": 1364409052}, "source": 13, "lastVis": 0, "type": 0, "event": 1364409052662964, author: {displayName: "sara",tags: [ ],profileUrl: "https://twitter.com/#!/135sara",avatar: "http://a0.twimg.com/profile_images/1349672055/Baqueira_29-01-2010_13-54-52_normal.jpg",type: 3,id: "123568642@twitter.com"}});
             var contentView = new ContentView({ content: livefyreContent });
@@ -21,6 +22,7 @@ function ($, jasmine, jasmineJquery, Content, LivefyreContent, ContentView) {
                 expect(typeof contentView.$el.find('.content-created-at').html()).toBe('string');
             });
         });
+
         describe('when viewing Content with no .createdAt', function () {
             var content = new Content('what'),
                 contentView = new ContentView({ content: content });
@@ -29,6 +31,7 @@ function ($, jasmine, jasmineJquery, Content, LivefyreContent, ContentView) {
                 expect(contentView.$el.find('.content-created-at').length).toBe(0);
             });
         });
+
         describe('when Content has no image attachment(s)', function() {
             var content = new Content('what'),
                 contentView = new ContentView({ content: content });
@@ -37,6 +40,7 @@ function ($, jasmine, jasmineJquery, Content, LivefyreContent, ContentView) {
                 expect(contentView.el).not.toHaveClass('content-with-image');
             });
         });
+
         describe('when Content has image attachment', function() {
             describe('when image attachment loads', function() {
                 var attachment = {
@@ -52,6 +56,7 @@ function ($, jasmine, jasmineJquery, Content, LivefyreContent, ContentView) {
                     expect(contentView.el).toHaveClass('content-with-image');
                 });
             });
+
             describe('when image attachment does not load', function() {
                 var attachment = {
                         provider_name: "bad provider",
