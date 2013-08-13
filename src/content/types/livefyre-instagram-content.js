265818ed283f28d96fb9a6828700fd53ac195573
Ryan Cheung <ryanc@livefyre.com>
2013-08-13 11:24:45 -0700

    Fix typo in jsdoc

--- src/content/types/livefyre-instagram-content.js
+++ src/content/types/livefyre-instagram-content.js
@@ -6,7 +6,7 @@ function(LivefyreContent, util) {
      * An instagram Content constructed from a StreamHub state of of 'feed' type
      *     that was transformed by lfcore.v2.procurement.feed.transformer.instagram
      * @param json {Object} A state object from StreamHub APIs
-     * @exports streamhub-sdk/content/types/livefyre-instgram-content
+     * @exports streamhub-sdk/content/types/livefyre-instagram-content
      * @constructor
      */
     var LivefyreInstagramContent = function(json) {
