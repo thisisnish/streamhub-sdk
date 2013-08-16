00d59ba444fc4ba50e75e4e3f1277f2eeed20a17
Benjamin Goering <bengoering@gmail.com>
2013-08-15 19:38:57 -0700

    Upgrade base64

--- requirejs.conf.js
+++ requirejs.conf.js
@@ -3,7 +3,7 @@ require.config({
   paths: {
     jquery: 'lib/jquery/jquery',
     text: 'lib/requirejs-text/text',
-    base64: 'lib/base64/base64',
+    base64: 'lib/base64/base64.min',
     hogan: 'lib/hogan/web/builds/2.0.0/hogan-2.0.0.amd',
     hgn: 'lib/requirejs-hogan-plugin/hgn',
     json: 'lib/requirejs-plugins/src/json',
