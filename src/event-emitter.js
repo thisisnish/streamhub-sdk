ea38c2d3607397c3f400b3c7e4665579d1db2cfb
Benjamin Goering <bengoering@gmail.com>
2013-08-26 14:45:26 -0700

    Remove files that are removed in v2.x

--- src/event-emitter.js
+++
@@ -1,7 +0,0 @@
-define(['event-emitter', 'streamhub-sdk/debug'], function(EventEmitter, debug) {
-    var log = debug('streamhub-sdk/event-emitter');
-    log("'streamhub-sdk/event-emitter' is deprecated. Please switch to " +
-        "'event-emitter'. You can find an implementation here: " +
-        "https://github.com/Livefyre/event-emitter")
-    return EventEmitter;
-});
