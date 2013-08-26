4d27976f1682c173f10a19d8cd4f74792063c273
Benjamin Goering <bengoering@gmail.com>
2013-08-26 14:47:16 -0700

    Remove all exports but .version from main.js

--- src/main.js
+++ src/main.js
@@ -1,11 +1,8 @@
 define([
     'streamhub-sdk/jquery',
-    'streamhub-sdk/auth',
     'streamhub-sdk/debug',
     'event-emitter',
     'streamhub-sdk/storage',
-    'streamhub-sdk/stream',
-    'streamhub-sdk/stream-manager',
     'streamhub-sdk/util',
     'streamhub-sdk/view',
     'streamhub-sdk/clients/livefyre-stream-client',
@@ -20,19 +17,13 @@ define([
     'streamhub-sdk/content/views/content-view',
     'streamhub-sdk/content/views/facebook-content-view',
     'streamhub-sdk/content/views/twitter-content-view',
-    'streamhub-sdk/streams/livefyre-stream',
-    'streamhub-sdk/streams/livefyre-reverse-stream',
-    'streamhub-sdk/stream-helpers/livefyre-helper',
     'streamhub-sdk/views/list-view',
     'text!streamhub-sdk/version.txt'
 ], function(
     $,
-    Auth,
     debug,
     EventEmitter,
     Storage,
-    Stream,
-    StreamManager,
     Util,
     View,
     LivefyreStreamClient,
@@ -47,52 +38,10 @@ define([
     ContentView,
     FacebookContentView,
     TwitterContentView,
-    LivefyreStream,
-    LivefyreReverseStream,
-    LivefyreHelper,
     ListView,
     VersionInfo
 ) {
-    var Hub = {};
-    Hub.debug = debug;
-    Hub.Auth = Auth;
-    Hub.EventEmitter = EventEmitter;
-    Hub.Storage = Storage;
-    Hub.Stream = Stream;
-    Hub.StreamManager = StreamManager;
-    Hub.Util = Util;
-    Hub.View = View;
-    
-    Hub.Clients = {};
-    Hub.Clients.LivefyreStreamClient = LivefyreStreamClient;
-    Hub.Clients.LivefyreBootstrapClient = LivefyreBootstrapClient;
-    Hub.Clients.LivefyreWriteClient = LivefyreWriteClient;
-
-    Hub.Content = Content;
-
-    Hub.Content.Types = {};
-    Hub.Content.Types.LivefyreContent = LivefyreContent;
-    Hub.Content.Types.LivefyreFacebookContent = LivefyreFacebookContent;
-    Hub.Content.Types.LivefyreTwitterContent = LivefyreTwitterContent;
-    Hub.Content.Types.Oembed = Oembed;
-    Hub.Content.Types.TwitterContent = TwitterContent;
-
-    Hub.Content.Views = {};
-    Hub.Content.Views.ContentView = ContentView;
-    Hub.Content.Views.FacebookContentView = FacebookContentView;
-    Hub.Content.Views.TwitterContentView = TwitterContentView;
-    
-    Hub.Streams = {};
-    Hub.Streams.LivefyreStream = LivefyreStream;
-    Hub.Streams.LivefyreReverseStream = LivefyreReverseStream;
-    
-    Hub.Streams.Helpers = {};
-    Hub.Streams.Helpers.LivefyreHelper = LivefyreHelper;
-
-    Hub.Views = {};
-    Hub.Views.ListView = ListView;
-    
-    Hub.version = $.trim(VersionInfo);
-
-    return Hub;
+    var exports = {};
+    exports.version = $.trim(VersionInfo);
+    return exports;
 });
