27d7e1fe92c63305c135511ace92b5f723230502
Benjamin Goering <bengoering@gmail.com>
2013-08-26 14:53:32 -0700

    Remove jasmine-spy-stream mock (unused and old stream interface). Remove other dependencies on streamhub-sdk/stream

--- tests/spec/view.js
+++ tests/spec/view.js
@@ -1,27 +1,7 @@
 define([
-    'streamhub-sdk/jquery',
     'jasmine',
-    'streamhub-sdk/view',
-    'streamhub-sdk/stream',
-    'jasmine-jquery',
-    'streamhub-sdk-tests/mocks/jasmine-spy-stream'],
-function ($, jasmine, View, Stream, jasminejquery, JasmineSpyStream) {
-    describe('A base View', function () {
-        var view, opts;
-
-        describe('when constructed', function () {
-            beforeEach(function() {
-                opts = {
-                    streams: {
-                        main: new Stream(),
-                        reverse: new Stream()
-                    }
-                };
-                opts.streams.main.start = jasmine.createSpy();
-                opts.streams.reverse.start = jasmine.createSpy();
-
-                view = new View(opts);
-            });
-        });
-    }); 
+    'streamhub-sdk/view'],
+function (jasmine, View) {
+    describe('streamhub-sdk/view', function () {
+    });
 });
