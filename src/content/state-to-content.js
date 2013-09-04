a1fec2bb4640e973f90e976b2eaa4eb52ae51fb6
Benjamin Goering <bengoering@gmail.com>
2013-09-04 06:16:56 -0700

    Start handling out of order children/parents in state-to-content

--- src/content/state-to-content.js
+++ src/content/state-to-content.js
@@ -2,9 +2,11 @@ define([
     'streamhub-sdk/content/types/livefyre-content',
     'streamhub-sdk/content/types/livefyre-twitter-content',
     'streamhub-sdk/content/types/livefyre-facebook-content',
-    'streamhub-sdk/content/types/livefyre-oembed'
+    'streamhub-sdk/content/types/oembed',
+    'streamhub-sdk/content/types/livefyre-oembed',
+    'streamhub-sdk/storage'
 ], function (LivefyreContent, LivefyreTwitterContent, LivefyreFacebookContent,
-LivefyreOembed) {
+Oembed, LivefyreOembed, Storage) {
 
 	/**
 	 * An Object that transforms state objects from Livefyre APIs
@@ -29,14 +31,56 @@ LivefyreOembed) {
      * @return {LivefyreContent} A new, correctly typed, content object. 
      */
 	StateToContent.transform = function (state, author) {
-        var sourceName = StateToContent.enums.source[state.source];
+        var isPublic = (typeof state.vis === 'undefined') || (state.vis === 1),
+            isReply = state.content.parentId,
+            isContent = (state.type === 0),
+            childStates = state.childContent || [],
+            childContent,
+            content,
+            childContent;
 
         // TODO: Non-vis states may still have childContent!
-        if (state.vis !== 1) {
+        if ( ! isPublic) {
         	return;
         }
 
-		state.author = author;
+        content = StateToContent._createContent(state, author);
+
+        // Store content with IDs in case we later get
+        // replies or attachments targeting it
+        if (content && content.id) {
+            Storage.set(content.id, content);
+        }
+
+        // Get child states (replies and attachments)
+        childStates = state.childContent || [];
+        // Transform child states (replies and attachments)
+        // This will put them in Storage
+        for (var i=0, numChildren=childStates.length; i < numChildren; i++) {
+            childContent = this.transform(childStates[i]);
+        }
+
+        // At this point, all content and children (recursively)
+        // Are stored by ID
+
+        // Attach attachments to their target, or store for later
+        
+        // Add replies to their parent, or store for later
+
+        // TODO: Allow for returning of replies
+        if (isReply) {
+            return;
+        }
+
+        return content;
+	}
+
+
+    StateToContent._createContent = function (state, author) {
+        var sourceName = StateToContent.enums.source[state.source];
+
+        state.author = author;
+
         if (state.type === 3) {
             return new LivefyreOembed(state);
         } else if (sourceName === 'twitter') {
@@ -46,7 +90,7 @@ LivefyreOembed) {
         } else if (['livefyre','feed'].indexOf(sourceName) !== -1) {
             return new LivefyreContent(state);
         }
-	}
+    };
 
 
     StateToContent.enums = {};
