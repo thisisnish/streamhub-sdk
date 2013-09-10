528ad190ca0dd1e287793b2b416c42e27e94e384
Benjamin Goering <bengoering@gmail.com>
2013-09-10 14:04:05 -0700

    Collection.pipe can be used to pipe to a writable, with a special case for writables with .more like ListViews

--- src/collection.js
+++ src/collection.js
@@ -1,46 +1,93 @@
 define([
-	'streamhub-sdk/streams/collection-archive',
-	'streamhub-sdk/streams/collection-updater'],
-function (CollectionArchive, CollectionUpdater) {
-	/**
-	 * An Object that represents a hosted StreamHub Collection
-	 */
-	function Collection (opts) {
-		this.network = opts.network;
-		this.siteId = opts.siteId;
-		this.articleId = opts.articleId;
-		this.environment = opts.environment;
-	}
+    'streamhub-sdk/streams/collection-archive',
+    'streamhub-sdk/streams/collection-updater',
+    'stream/passthrough',
+    'inherits'],
+function (CollectionArchive, CollectionUpdater, PassThrough, inherits) {
+    /**
+     * An Object that represents a hosted StreamHub Collection
+     */
+    function Collection (opts) {
+        this.network = opts.network;
+        this.siteId = opts.siteId;
+        this.articleId = opts.articleId;
+        this.environment = opts.environment;
+
+        // Internal streams
+        this._updater = null;
+        this._pipedArchives = [];
+
+        PassThrough.call(this, opts);
+    }
+    inherits(Collection, PassThrough);
+
+    /**
+     * Create a readable stream that will read through the Collection Archive
+     * The Collection Archive contains older Content in the Collection
+     * @param opts {object}
+     * @param [opts.bootstrapClient] {BootstrapClient} A bootstrapClient to
+     *     construct the CollectionArchive with
+     * @returns {streamhub-sdk/streams/collection-archive}
+     */
+    Collection.prototype.createArchive = function (opts) {
+        opts = opts || {};
+        return new CollectionArchive({
+            network: this.network,
+            siteId: this.siteId,
+            articleId: this.articleId,
+            environment: this.environment,
+            bootstrapClient: opts.bootstrapClient
+        });
+    };
 
 
-	/**
-	 * Create a readable stream that will read through the Collection Archive
-	 * The Collection Archive contains older Content in the Collection
-	 * @returns {streamhub-sdk/streams/collection-archive}
-	 */
-	Collection.prototype.createArchive = function () {
-		return new CollectionArchive({
-			network: this.network,
-			siteId: this.siteId,
-			articleId: this.articleId,
-			environment: this.environment
-		});
-	};
+    /**
+     * Create a Readable Stream that will stream any new updates to the
+     * collection like additions, removals, edits, etc.
+     */
+    Collection.prototype.createUpdater = function () {
+        return new CollectionUpdater({
+            network: this.network,
+            siteId: this.siteId,
+            articleId: this.articleId,
+            environment: this.environment,
+        });
+    };
 
 
-	/**
-	 * Create a Readable Stream that will stream any new updates to the
-	 * collection like additions, removals, edits, etc.
-	 */
-	Collection.prototype.createUpdater = function () {
-		return new CollectionUpdater({
-			network: this.network,
-			siteId: this.siteId,
-			articleId: this.articleId,
-			environment: this.environment	
-		});
-	};
+    /**
+     * Pipe updates in the Collection the passed destination Writable
+     * @param writable {Writable} The destination to pipe udpates to
+     * @param opts {object}
+     * @param [opts.pipeArchiveToMore=true] Whether to try to pipe
+     *     a CollectionArchive to writable.more, if it is also writable
+     *     This is helpful when piping to a ListView
+     */
+    Collection.prototype.pipe = function (writable, opts) {
+        var self = this,
+            archive;
+        opts = opts || {};
+        if (typeof opts.pipeArchiveToMore === 'undefined') {
+            opts.pipeArchiveToMore = true;
+        }
+
+        // Create an internal updater the first time the Collection is piped
+        if ( ! this._updater) {
+            this._updater = this.createUpdater();
+            this._updater.pipe(this);
+        }
+
+        PassThrough.prototype.pipe.apply(this, arguments);
+
+        // If piped to a ListView (or something with a .more),
+        // pipe an archive to .more
+        if (opts.pipeArchiveToMore && writable.more && writable.more.writable) {
+            archive = this.createArchive()
+            archive.pipe(writable.more);
+            this._pipedArchives.push(archive);
+        }
+    };
 
 
-	return Collection;
+    return Collection;
 });
--- tests/spec/collection.js
+++ tests/spec/collection.js
@@ -1,52 +1,98 @@
 define([
-	'jasmine',
-	'streamhub-sdk/collection',
-	'streamhub-sdk/streams/collection-archive',
-	'streamhub-sdk/streams/collection-updater'
-], function (jasmine, Collection, CollectionArchive, CollectionUpdater) {
-	describe('streamhub-sdk/collection', function () {
-		it('is a function', function () {
-			expect(Collection).toEqual(jasmine.any(Function));
-		});
-		describe('instance', function () {
-			var opts,
-				collection;
-			beforeEach(function () {
-				opts = {
-					network: 'test.fyre.co',
-					siteId: 'testSiteId',
-					articleId: 'testArticleId',
-					environment: 'test.livefyre.com'
-				};
-				collection = new Collection(opts);
-			});
+    'jasmine',
+    'streamhub-sdk/collection',
+    'streamhub-sdk/streams/collection-archive',
+    'streamhub-sdk/streams/collection-updater',
+    'streamhub-sdk/views/list-view',
+    'stream/writable',
+    'stream/readable'
+], function (jasmine, Collection, CollectionArchive, CollectionUpdater, ListView,
+Writable, Readable) {
+    describe('streamhub-sdk/collection', function () {
+        it('is a function', function () {
+            expect(Collection).toEqual(jasmine.any(Function));
+        });
+        describe('instance', function () {
+            var opts,
+                collection;
+            beforeEach(function () {
+                opts = {
+                    network: 'test.fyre.co',
+                    siteId: 'testSiteId',
+                    articleId: 'testArticleId',
+                    environment: 'test.livefyre.com'
+                };
+                collection = new Collection(opts);
+            });
 
-			it('has .network', function () {
-				expect(collection.network).toBe(opts.network);
-			});
-			it('has .siteId', function () {
-				expect(collection.siteId).toBe(opts.siteId);
-			});
-			it('has .articleId', function () {
-				expect(collection.articleId).toBe(opts.articleId);
-			});
-			it('has .environment', function () {
-				expect(collection.environment).toBe(opts.environment);
-			});
+            it('has .network', function () {
+                expect(collection.network).toBe(opts.network);
+            });
+            it('has .siteId', function () {
+                expect(collection.siteId).toBe(opts.siteId);
+            });
+            it('has .articleId', function () {
+                expect(collection.articleId).toBe(opts.articleId);
+            });
+            it('has .environment', function () {
+                expect(collection.environment).toBe(opts.environment);
+            });
 
-			describe('.createArchive', function () {
-				it('returns a readable CollectionArchive Stream', function () {
-					var archive = collection.createArchive();
-					expect(archive instanceof CollectionArchive).toBe(true);
-				});
-			});
+            describe('.createArchive', function () {
+                it('returns a readable CollectionArchive Stream', function () {
+                    var archive = collection.createArchive();
+                    expect(archive instanceof CollectionArchive).toBe(true);
+                });
+            });
 
-			describe('.createUpdater', function () {
-				it('returns a readable CollectionUpdater Stream', function () {
-					var updater = collection.createUpdater();
-					expect(updater instanceof CollectionUpdater).toBe(true);
-				});
-			});
-		});
-	});
+            describe('.createUpdater', function () {
+                it('returns a readable CollectionUpdater Stream', function () {
+                    var updater = collection.createUpdater();
+                    expect(updater instanceof CollectionUpdater).toBe(true);
+                });
+            });
+
+            describe('.pipe(writable)', function () {
+                var writable,
+                    listView;
+                beforeEach(function () {
+                    writable = new Writable();
+                    listView = new ListView();
+                    collection.createUpdater = function () {
+                        var readable = new Readable();
+                        readable._read = function () { this.push(null); };
+                        return readable;
+                    };
+                    collection.createArchive = function () {
+                        // Use a Dummy BootstrapClient to prevent requests
+                        return Collection.prototype.createArchive.call(this, {
+                            bootstrapClient: {
+                                getContent: function (opts, errback) {
+                                    errback(null, {});
+                                }
+                            }
+                        });
+                    };
+                    writable._write = function (chunk, done) { done(); }
+                });
+                it('can be piped to a writable', function () {
+                    collection.pipe(writable);
+                });
+                describe('when piped to a ListView', function () {
+                    it('pipes an updater to ListView', function () {
+                        var onPipe = jasmine.createSpy('pipe');
+                        listView.on('pipe', onPipe);
+                        collection.pipe(listView);
+                        expect(onPipe).toHaveBeenCalled();
+                    });
+                    it('pipes an archive to ListView#more', function () {
+                        var onPipeToMore = jasmine.createSpy('pipe to more');
+                        listView.more.on('pipe', onPipeToMore);
+                        collection.pipe(listView);
+                        expect(onPipeToMore).toHaveBeenCalledWith(jasmine.any(CollectionArchive));
+                    });
+                });
+            });
+        });
+    });
 });
