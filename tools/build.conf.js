c5c7c88a4a275034a8f1776385415976ba10ab6b
Benjamin Goering <bengoering@gmail.com>
2013-11-12 02:56:35 -0800

    Don't build with license comments (jquerys source map creates 404s). Generate source maps.

--- tools/build.conf.js
+++ tools/build.conf.js
@@ -22,12 +22,14 @@
     excludeHogan: true
   },
   optimize: "uglify2",
+  preserveLicenseComments: false,
   uglify2: {
     compress: {
       unsafe: true
     },
     mangle: true
   },
+  generateSourceMaps: true,
   onBuildRead: function(moduleName, path, contents) {
     if (moduleName == "jquery") {
       contents = "define([], function(require, exports, module) {" + contents + "});";
