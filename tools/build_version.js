27035a13605a8309accf61fc54e6790041c027c5
Benjamin Goering <bengoering@gmail.com>
2013-12-20 15:58:24 -0800

    Change tools/build_version to use bower.json not component.json

--- tools/build_version.js
+++ tools/build_version.js
@@ -1,8 +1,8 @@
 var fs = require('fs');
 if (process.env.BUILD_NUMBER) {
-    var component = JSON.parse(fs.readFileSync('component.json'));
+    var component = JSON.parse(fs.readFileSync('bower.json'));
 
     component.version = component.version + (process.env.BUILD_EXT || "");
 
-    fs.writeFileSync('component.json', JSON.stringify(component, null, "  "));
+    fs.writeFileSync('bower.json', JSON.stringify(component, null, "  "));
 }
