be21fea55ff43041874810401ae3b4aababf25ce
Benjamin Goering <bengoering@gmail.com>
2013-12-23 17:59:34 -0600

    Add junit to karma config

--- tools/karma.conf.js
+++ tools/karma.conf.js
@@ -35,7 +35,7 @@ module.exports = function(config) {
 
     // test results reporter to use
     // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
-    reporters: ['dots'],
+    reporters: ['dots', 'junit'],
 
 
     // web server port
