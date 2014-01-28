404bbef25d850abc94f8ed616f901d1e467c68bc
Ross Pfahler <ross.pfahler@gmail.com>
2014-01-28 10:13:22 -0800

    Use fork of the Base64.js library that does not introduce globals into the IE namespace.

--- src/collection/clients/bootstrap-client.js
+++ src/collection/clients/bootstrap-client.js
@@ -45,7 +45,7 @@ function(LivefyreHttpClient, inherits, base64) {
             "/",
             opts.siteId,
             "/",
-            btoa(opts.articleId),
+            base64.btoa(opts.articleId),
             "/",
             typeof opts.page !== 'undefined' ? opts.page+'.json' : "init"
         ].join("");
