0c17173ed9f0f38b9f6aa5c27490f60eb192a7a2
Ryan Cheung <ryanc@livefyre.com>
2014-02-11 13:22:03 -0800

    Rename on opt to enabled for HubToggleButton

--- src/ui/button/hub-toggle-button.js
+++ src/ui/button/hub-toggle-button.js
@@ -6,23 +6,23 @@ var inherits = require('inherits');
 function HubToggleButton (fnOrCommand, opts) {
     opts = opts || {};
     opts.elClassPrefix = opts.elClassPrefix || '';
-    this._on = opts.on || false;
-    opts.elClassPrefix += this._on ? ' hub-btn-toggle-on' : 'hub-btn-toggle-off';
+    this._enabled = opts.enabled || false;
+    opts.elClassPrefix += this._enabled ? ' hub-btn-toggle-on' : 'hub-btn-toggle-off';
 
     HubButton.call(this, fnOrCommand, opts);
 }
 inherits(HubToggleButton, HubButton);
 
 HubToggleButton.prototype._execute = function () {
-    this._on = !this._on;
+    this._enabled = !this._enabled;
     HubButton.prototype._execute.call(this);
     this.$el.removeClass('hub-btn-toggle-on').removeClass('hub-btn-toggle-off');
-    this._on ? this.$el.addClass('hub-btn-toggle-on') : this.$el.addClass('hub-btn-toggle-off');
+    this._enabled ? this.$el.addClass('hub-btn-toggle-on') : this.$el.addClass('hub-btn-toggle-off');
 };
 
 HubToggleButton.prototype.getTemplateContext = function () {
     var context = HubButton.prototype.getTemplateContext.call(this);
-    context.on = this._on;
+    context.on = this._enabled;
 
     return context;
 };
