174cdfcb7fad875d11ed637b643d20fc6ba3af9f
Ryan Cheung <ryanc@livefyre.com>
2014-04-10 20:57:39 -0700

    Remove #render from InstagramContentView

--- src/content/views/instagram-content-view.js
+++ src/content/views/instagram-content-view.js
@@ -25,31 +25,6 @@ function (LivefyreContentView, Button, inherits) {
         }
     });
 
-    InstagramContentView.prototype.render = function () {
-        LivefyreContentView.prototype.render.call(this);
-
-        if (! this._rendered) {
-            var likeButton = new Button(undefined, {
-                elClassPrefix: 'hub',
-                className: 'hub-content-like'
-            });
-            var shareButton = new Button(undefined, {
-                elClassPrefix: 'hub',
-                className: 'hub-btn-link hub-content-share',
-                label: 'Share'
-            });
-
-            this.addButton(likeButton);
-            this.addButton(shareButton);
-        } else {
-            for (var i=0; i < this._controls['left'].length; i++) {
-                this.addButton(this._controls['left'][i]);
-            }
-        }
-
-        this._rendered = true;
-    };
-
     /**
      * Gets the template rendering context. By default, returns "this.content".
      * @return {Content} The content object this view was instantiated with.
