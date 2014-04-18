9ff243c3e9ff226371b2365503525405b4db1cf5
Benjamin Goering <bengoering@gmail.com>
2014-04-18 01:17:30 -0700

    first start

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
-define(['streamhub-sdk/jquery', 'text!streamhub-sdk/version.txt'],
-function($, version) {
+define([
+    'streamhub-sdk/jquery',
+    'text!streamhub-sdk/version.txt',
+    'streamhub-sdk/collection'],
+function($, version, Collection) {
     'use strict';
 
     return {
-        version: $.trim(version)
+        version: $.trim(version),
+        Collection: Collection
     };
 });
