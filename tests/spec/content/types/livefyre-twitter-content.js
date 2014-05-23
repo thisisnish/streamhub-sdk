a0c75aa9adf576ff21d9e7179a861351ff33e171
Benjamin Goering <bengoering@gmail.com>
2014-05-23 15:40:29 -0700

    Add tests for verified twitter user indicator

---
+++ tests/spec/content/types/livefyre-twitter-content.js
@@ -0,0 +1,19 @@
+define([
+    'streamhub-sdk/jquery',
+    'streamhub-sdk/content/types/livefyre-twitter-content',
+    'json!streamhub-sdk-tests/fixtures/verified-tweet-state.json',
+    'json!streamhub-sdk-tests/fixtures/unverified-tweet-state.json'],
+function ($, LivefyreTwitterContent, verifiedTweetState, unverifiedTweetState) {
+    'use strict';
+
+    describe('A LivefyreTwitterContent object', function () {
+        it('has .twitterVerified if the content author is a verified twitter user', function () {
+            var content = new LivefyreTwitterContent(verifiedTweetState);
+            expect(content.twitterVerified).toBe(true);
+        });
+        it('is not .twitterVierifed if the content author is not a verified twitter user', function () {
+            var content = new LivefyreTwitterContent(unverifiedTweetState);
+            expect(content.twitterVerified).toBe(false);
+        })
+    });
+});
