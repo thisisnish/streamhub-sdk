454a9dd96538f76ec8896eeef691f39e26039b62
Ryan Cheung <ryanc@livefyre.com>
2014-06-18 14:54:15 -0700

    Add hasTheme mixin

--- src/content/views/card-content-view.js
+++ src/content/views/card-content-view.js
@@ -3,6 +3,7 @@ var ContentView = require('streamhub-sdk/content/views/content-view');
 var asCardContentView = require('streamhub-sdk/content/views/mixins/card-content-view-mixin');
 
 var CardContentView = function (opts) {
+    opts = opts || {};
     ContentView.call(this, opts);
     asCardContentView(this);
 };
---
+++ src/content/views/mixins/theme-mixin.js
@@ -0,0 +1,19 @@
+'use strict';
+
+function hasTheme(themeClassName, contentView) {
+    if (! themeClassName) {
+        return;
+    }
+
+    /**
+     * Render the content inside of the LivefyreContentView's element.
+     * @returns {LivefyreContentView}
+     */
+    var oldRender = contentView.render;
+    contentView.render = function () {
+        oldRender.apply(contentView, arguments);
+        contentView.$el.addClass(themeClassName);
+    };
+};
+
+module.exports = hasTheme;
