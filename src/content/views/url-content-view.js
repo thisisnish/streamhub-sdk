fdf96354c7a56ad02c62828da07f793542f658af
Joseph Ross <jross@livefyre.com>
2014-07-10 09:23:02 -0700

    Some style updates

--- src/content/views/url-content-view.js
+++ src/content/views/url-content-view.js
@@ -1,12 +1,10 @@
 var inherits = require('inherits');
-var View = require('streamhub-sdk/view');
 var LivefyreContentView = require('streamhub-sdk/content/views/livefyre-content-view');
 var asUrlContentView = require('streamhub-sdk/content/views/mixins/url-content-view-mixin');
 
 'use strict';
 
 var UrlContentView = function (opts) {
-    //opts.footerView = new View();
     LivefyreContentView.call(this, opts);
     asUrlContentView(this, opts);
 };
