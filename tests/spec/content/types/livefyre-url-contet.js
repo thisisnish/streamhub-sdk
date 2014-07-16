442e935215ce2c46bc47ace2d5e6911e16524809
Joseph Ross <jross@livefyre.com>
2014-07-16 12:10:48 -0700

    Fixed tests. Replaced old faked out fixture data with real data

--- tests/spec/content/types/livefyre-url-contet.js
+++ tests/spec/content/types/livefyre-url-contet.js
@@ -8,26 +8,26 @@ function ($, LivefyreUrlContent, urlTypeContent) {
     describe('A LivefyreUrlContent object', function () {
         it('has .viaText is displayName on generator if present', function () {
             var content = new LivefyreUrlContent(urlTypeContent);
-            expect(content.viaText).toBe("Google");
+            expect(content.viaText).toBe("Nytimes");
         });
 
         it('has .viaText is id on generator if displayName is not present', function () {
             var modUrl = urlTypeContent;
-            modUrl.generator.displayName = undefined;
+            modUrl.content.generator.displayName = undefined;
 
             var content = new LivefyreUrlContent(modUrl);
             
-            expect(content.viaText).toBe("google.com");
+            expect(content.viaText).toBe("www.nytimes.com");
         });
 
         it('has .viaText is the url on generator if no other option is present', function () {
             var modUrl = urlTypeContent;
-            modUrl.generator.displayName = undefined;
-            modUrl.generator.id = undefined;
+            modUrl.content.generator.displayName = undefined;
+            modUrl.content.generator.id = undefined;
 
             var content = new LivefyreUrlContent(modUrl);
             
-            expect(content.viaText).toBe("http://news.google.com");
+            expect(content.viaText).toBe("http://www.nytimes.com");
         });
         
         it('has .title if oembed has one', function () {
