3e17b30754848dfc1ecacfdf8e241ccad85d624c
Mark Doten <mark.doten@gmail.com>
2015-11-24 11:30:45 -0800

    LF-20449: Converting youtube content to a youtube view
    https://livefyre.atlassian.net/browse/LF-20449

--- src/content/types/livefyre-youtube-content.js
+++ src/content/types/livefyre-youtube-content.js
@@ -7,9 +7,10 @@ define([
     function LivefyreYoutubeContent(json) {
         json.content = Object.create(json.content);
         json.content.generator = {
-            url: "https://www.youtube.com/",
-            displayName: "YouTube",
-            id: "www.youtube.com"
+            displayName: 'YouTube',
+            id: 'www.youtube.com',
+            image: 'https://www.google.com/s2/favicons?domain=youtube.com',
+            url: 'https://www.youtube.com/'
         };
         LivefyreUrlContent.call(this, json);
     }
