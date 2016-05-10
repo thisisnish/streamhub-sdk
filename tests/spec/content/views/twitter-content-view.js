7ecb7e40be76d519d016a2c6c6d728165ed803e8
raviranjan-tudip <ravi.ranjan@tudip.nl>
2016-05-10 17:34:18 +0530

    Added test cases.

--- tests/spec/content/views/twitter-content-view.js
+++ tests/spec/content/views/twitter-content-view.js
@@ -1,10 +1,11 @@
 define([
     'streamhub-sdk/jquery',
+    'streamhub-sdk/content/content-view-factory',
     'streamhub-sdk/content/types/livefyre-twitter-content',
     'streamhub-sdk/content/views/twitter-content-view',
     'json!streamhub-sdk-tests/fixtures/verified-tweet-state.json',
     'json!streamhub-sdk-tests/fixtures/unverified-tweet-state.json'],
-function ($, LivefyreTwitterContent, TwitterContentView, verifiedTweetState, unverifiedTweetState) {
+function ($, ContentViewFactory, LivefyreTwitterContent, TwitterContentView, verifiedTweetState, unverifiedTweetState) {
     'use strict';
 
     describe('A TwitterContentView object', function () {
@@ -25,6 +26,27 @@ function ($, LivefyreTwitterContent, TwitterContentView, verifiedTweetState, unv
             var contentView = new TwitterContentView({ content: content });
             contentView.render();
             expect(contentView.$('.content-source-logo').attr('href')).toBe('https://twitter.com');
+        });
+    });
+
+    describe('Expand button', function () {
+        it("renders only if showExpandButton is set to true", function () {
+            var contentViewFactory = new ContentViewFactory({showExpandButton: true});
+            var content = new LivefyreTwitterContent(verifiedTweetState);
+            var twitterContentView = contentViewFactory.createContentView(content);
+            twitterContentView.render();
+            expect(twitterContentView.$el.find('.hub-content-action-expand')).toHaveLength(1);
+        });
+        it("does not render if showExpandButton is not set", function () {
+            var contentViewFactory = new ContentViewFactory();
+            var contentViewFactory1 = new ContentViewFactory({showExpandButton: false});
+            var content = new LivefyreTwitterContent(verifiedTweetState);
+            var twitterContentView = contentViewFactory.createContentView(content);
+            twitterContentView.render();
+            var twitterContentView1 = contentViewFactory1.createContentView(content);
+            twitterContentView1.render();
+            expect(twitterContentView.$el.find('.hub-content-action-expand')).toHaveLength(0);
+            expect(twitterContentView1.$el.find('.hub-content-action-expand')).toHaveLength(0);
         });
     });
 });
