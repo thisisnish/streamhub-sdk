eec33e74d1f8ede0619a1371443dccddb8f9eb75
Kyle Taborski <ktaborski@livefyre.com>
2017-02-24 12:38:13 -0800

    rendering products into body instead of as peer

--- src/content/views/product-carousel-view.js
+++ src/content/views/product-carousel-view.js
@@ -1,6 +1,7 @@
 var template = require('hgn!streamhub-sdk/content/templates/product-carousel');
 var CompositeView = require('view/composite-view');
 var ProductBlockView = require('streamhub-sdk/content/views/product-block-view');
+var View = require('view/view');
 var inherits = require('inherits');
 
 'use strict';
@@ -30,14 +31,28 @@ ProductCarouselView.prototype.listClass = 'product-carousel-list';
  * @param {Object} opts
  * @param {boolean=} shouldRender
  */
-ProductCarouselView.prototype._addInitialChildViews = function (opts, shouldRender) {
-    shouldRender = shouldRender || false;
+ProductCarouselView.prototype._addInitialChildViews = function (opts) {
     for (var i = 0; opts.content.products && i < opts.content.products.length; i++) {
         this.add(new ProductBlockView({
             product: opts.content.products[i],
             buyButtonText: opts.buyButtonText
-        }), { render: shouldRender });
+        }));
     }
+};
+
+ProductCarouselView.prototype.add = function (view) {
+    this._childViews.push(view);
+    return this;
+};
+
+ProductCarouselView.prototype.render = function (view, opts) {
+    View.prototype.render.call(this);
+    for (var i=0; i < this._childViews.length; i++) {
+        var childView = this._childViews[i];
+        this.$el.find('.' + this.listClass).append(childView.el);
+        childView.render();
+    }
+    return this;
 };
 
 ProductCarouselView.prototype.getTemplateContext = function () {
