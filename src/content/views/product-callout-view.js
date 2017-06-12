7e535a9180514a3c17bbe7d3b68ad8bffd881261
Mark Doten <mark.doten@gmail.com>
2017-06-12 11:57:19 -0700

    FYR-1873: Formatting + minor fixes

--- src/content/views/product-callout-view.js
+++ src/content/views/product-callout-view.js
@@ -5,11 +5,10 @@ var View = require('view');
 var ProductCarouselView = require('streamhub-sdk/content/views/product-carousel-view');
 var Popover = require('streamhub-ui/popover');
 
-
 'use strict';
 
 function ProductCalloutView(opts) {
-	opts.dataAttr = opts.dataAttr || {};
+    opts.dataAttr = opts.dataAttr || {};
     View.call(this, opts);
 }
 inherits(ProductCalloutView, View);
@@ -27,32 +26,31 @@ ProductCalloutView.prototype.getTemplateContext = function () {
 };
 
 ProductCalloutView.prototype.createPopover = function () {
-	
-	if (this.popover) {
-		document.getElementById('popover-div').appendChild(this.popover.el);
-		this.popover.resizeAndReposition(this.el.children[0]);
-		this.popover.positionArrowSmart(this.el.children[0]);
-		return;
-	}
+    if (this.popover) {
+        document.getElementById('popover-div').appendChild(this.popover.el);
+        this.popover.resizeAndReposition(this.el.children[0]);
+        this.popover.positionArrowSmart(this.el.children[0]);
+        return;
+    }
 
-	var product_popup = new ProductCarouselView($.extend({cardsInView: 2}, this.opts));
-	product_popup.render();
+    var product_popup = new ProductCarouselView($.extend({cardsInView: 2}, this.opts));
+    product_popup.render();
 
-	var width = this.$el.width();
+    var width = this.$el.width();
 
-	var $el = $(document.createElement('div'));
-	$el.attr("id", "popover-div");
-	$el.attr(this.opts.dataAttr);
-	var $childEl = $(document.createElement('div'));
-	$el.append($childEl);
-	$el.appendTo(document.body);
+    var $el = $(document.createElement('div'));
+    $el.attr("id", "popover-div");
+    $el.attr(this.opts.dataAttr);
+    var $childEl = $(document.createElement('div'));
+    $el.append($childEl);
+    $el.appendTo(document.body);
 
-	this.popover = new Popover({
+    this.popover = new Popover({
         maxWidth: width + 10,
         minWidth: width + 10,
         el: $childEl
     });
-	
+    
     this.popover.$el.addClass('lf-product-popover');
     this.popover._position = Popover.POSITIONS.SMART_TOP;
     this.popover.setContentNode(product_popup.el);
@@ -61,8 +59,8 @@ ProductCalloutView.prototype.createPopover = function () {
     this.popover.positionArrowSmart(this.el.children[0]); 
 
     this.popover.$el.on("mouseleave", function() {
-		$(this).detach();
-	});
+        // $(this).detach();
+    });
 };
 
 
@@ -72,13 +70,13 @@ ProductCalloutView.prototype.events = View.prototype.events.extended({
     },
 
     'mouseleave': function(e) {
-    	hidePopover = (function () {
-    		this.detach();
-		}).bind(this.popover.$el);
+        // hidePopover = (function () {
+        //     this.detach();
+        // }).bind(this.popover.$el);
 
-		var tOut = setTimeout(function() {hidePopover()}.bind(this.popover.$el), 100);
-		
-		$(this.popover.$el).hover(function() { clearTimeout(tOut); });
+        // var tOut = setTimeout(function() {hidePopover()}.bind(this.popover.$el), 100);
+        
+        // $(this.popover.$el).hover(function() { clearTimeout(tOut); });
     } 
 });
 
